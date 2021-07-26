e569766ed5f309d5fdb2fce86b6e8c43a5a344cb
Luke Xue <lukexue064@gmail.com>
2021-07-25 21:21:00 -0500

    Initial Commit

---
+++ Mortgage Calculator/Mortgage_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mortgage_CalculatorApp.swift
+//  Mortgage Calculator
+//
+//  Created by Luke Xue on 7/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Mortgage_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
